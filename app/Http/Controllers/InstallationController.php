553f374367cdc1edc94eecf0bc8baf52e59e7996
gerli-arce <yoninfantearce@gmail.com>
2023-02-13 13:54:48 -0500

    nts

---
+++ app/Http/Controllers/InstallationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\gLibraries\gJSON;
+use App\gLibraries\gTrace;
+use App\gLibraries\guid;
+use App\gLibraries\gValidate;
+use App\Models\Response;
+use App\Models\User;
+use App\Models\ViewUsers;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class InstallationController extends Controller
+{
+    
+}
