d0a23d4d52963eda2d6f14d391fea91028c9f4f7
gerli-arce <yoninfantearce@gmail.com>
2023-02-16 12:02:51 -0500

    nts

---
+++ app/Http/Controllers/SalesProductsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\gLibraries\gJson;
+use App\gLibraries\gTrace;
+use App\gLibraries\gUid;
+use App\gLibraries\gValidate;
+use App\Models\Branch;
+use App\Models\EntryProducts;
+use App\Models\Product;
+use App\Models\Response;
+use App\Models\ViewProducts;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SalesProductsController extends Controller
+{
+    public function registerInstallation(Request $request){
+        
+    }
+}
