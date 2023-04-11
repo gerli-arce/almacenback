073774f7604719647cd234005b5a6a781fceb090
gerli-arce <yoninfantearce@gmail.com>
2023-04-11 09:28:03 -0500

    nts

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,9 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
+            // 'throttle:api',
+            'throttle:500,1',
+            // 'bindings',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
