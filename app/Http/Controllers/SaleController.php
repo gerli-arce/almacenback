a94b68d0e82790d1c2dcbcbf70d0353f60ce46eb
gerli-arce <yoninfantearce@gmail.com>
2023-08-16 14:01:42 -0500

    nts

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -60,7 +60,7 @@ class SaleController extends Controller
             $salesProduct->price_installation = $request->price_sale;
             $salesProduct->discount = $request->discount;
             $salesProduct->type_pay = "CONTADO";
-            $salesProduct->mount_dues = 1;
+            $salesProduct->mount_dues = $request->mount_dues;
             if (isset($request->description)) {
                 $salesProduct->description = $request->description;
             }
@@ -277,6 +277,11 @@ class SaleController extends Controller
             if (isset($request->price_installation)) {
                 $salesProduct->price_installation = $request->price_installation;
             }
+            if (isset($request->mount_dues)) {
+                $salesProduct->mount_dues = $request->mount_dues;
+            }
+
+            
             if (isset($request->type_intallation)) {
                 $salesProduct->type_intallation = $request->type_intallation;
             }
