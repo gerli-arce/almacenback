770757a2ae4bcb482bf53d0e53e0a76fa230bcc6
gerli-arce <yoninfantearce@gmail.com>
2023-09-08 15:46:57 -0500

    nts

--- app/Models/ExcelExport.php
+++ app/Models/ExcelExport.php
@@ -7,7 +7,6 @@ use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Events\AfterSheet;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
-use PhpOffice\PhpSpreadsheet\Style\Font;
 
 class ExcelExport implements FromCollection, WithHeadings, WithEvents
 {
@@ -24,30 +23,30 @@ class ExcelExport implements FromCollection, WithHeadings, WithEvents
     {
         return [
             [
-                'CONTROL DE GUIAS Y FACTURAS DE COMPRA ALMACEN MES DE '.$this->month,
+                'CONTROL DE GUIAS Y FACTURAS DE COMPRA ALMACEN MES DE ' . $this->month,
                 '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',
             ],
             [
-                'CONTROL DE TRANSPORTE', '', '', '','', 'CONTROL DE GUIA DE PROVEEDOR', '', '', '','', 'CONTROL DE FACTURA', '', '', '', '', '', '', 'PRECIO AL 35%', '', '',
+                'CONTROL DE TRANSPORTE', '', '', '', '', 'CONTROL DE GUIA DE PROVEEDOR', '', '', '', '', 'CONTROL DE FACTURA', '', '', '', '', '', '', 'PRECIO AL 35%', '', '',
             ],
             [
                 'FECHA DE ENVIO',
                 'Nº DE COMPRPOBANTE',
-                'EMPRESA DE TRANSPORTE', 
-                'PRECIO', 
-                'FECHA DE RECOJO', 
-                'Nº DE GUIA', 
-                'PROVEEDOR', 
-                'DESCRIPCION', 
-                'MEDIDA', 
+                'EMPRESA DE TRANSPORTE',
+                'PRECIO',
+                'FECHA DE RECOJO',
+                'Nº DE GUIA',
+                'PROVEEDOR',
+                'DESCRIPCION',
+                'MEDIDA',
                 'CANTIDAD',
                 'Nº DE FACTURA',
-                'EMPRESA DESIGNADA', 
+                'EMPRESA DESIGNADA',
                 'VALOR UNITARIO',
-                'SUB TOTAL', 
+                'SUB TOTAL',
                 'IGV',
-                'PRECIO CON IGV', 
-                'TOTAL', 
+                'PRECIO CON IGV',
+                'TOTAL',
                 'PRECIO UNIT CON IGV',
                 '35%',
                 'PRECIO TOTAL',
@@ -59,33 +58,62 @@ class ExcelExport implements FromCollection, WithHeadings, WithEvents
     {
         return [
             AfterSheet::class => function (AfterSheet $event) {
-
                 // Combinar celdas para el encabezado principal CONTROL DE GUIAS Y FACTURAS DE COMPRA ALMACEN MES DE (horizontal y vertical)
-                $event->sheet->getDelegate()->mergeCells('A1:S1');
-                $event->sheet->getDelegate()->getStyle('A1:S1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+                $event->sheet->getDelegate()->mergeCells('A1:T1');
+                $event->sheet->getDelegate()->getStyle('A1:T1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+                // $event->sheet->getDelegate()->getStyle('A1:T1')->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+                $event->sheet->getDelegate()->getStyle('A1:T1')->getBorders()->getTop()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+                $event->sheet->getDelegate()->getStyle('A1:T1')->getBorders()->getBottom()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+                $event->sheet->getDelegate()->getStyle('A1:T1')->getBorders()->getLeft()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+                $event->sheet->getDelegate()->getStyle('A1:T1')->getBorders()->getRight()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+                $event->sheet->getDelegate()->getStyle('A1:T1')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('FFFF00'); // Color de fondo amarillo
+                $event->sheet->getDelegate()->getStyle('A1:T1')->getFont()->getColor()->setARGB('000000');
+                $event->sheet->getDelegate()->getStyle('A1:T1')->getFont()->setSize(22);
 
                 // Combinar celdas para el encabezado principal CONTROL DE TRANSPORTE
                 $event->sheet->getDelegate()->mergeCells('A2:E2');
                 $event->sheet->getDelegate()->getStyle('A2:E2')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+                $event->sheet->getDelegate()->getStyle('A2:E2')->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+                $event->sheet->getDelegate()->getStyle('A2:E2')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('40FF00'); // Color de fondo amarillo
+                $event->sheet->getDelegate()->getStyle('A2:E2')->getFont()->getColor()->setARGB('000000');
+                $event->sheet->getDelegate()->getStyle('A2:E2')->getFont()->setSize(18);
 
                 // Combinar celdas para la segunda fila del encabezado CONTROL DE GUIA DE PROVEEDOR
                 $event->sheet->getDelegate()->mergeCells('F2:J2');
                 $event->sheet->getDelegate()->getStyle('F2:J2')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+                $event->sheet->getDelegate()->getStyle('F2:J2')->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+                $event->sheet->getDelegate()->getStyle('F2:J2')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('2E64FE'); // Color de fondo amarillo
+                $event->sheet->getDelegate()->getStyle('F2:J2')->getFont()->getColor()->setARGB('000000');
+                $event->sheet->getDelegate()->getStyle('F2:J2')->getFont()->setSize(18);
 
                 // Combinar celdas para la segunda fila del encabezado CONTROL DE FACTURA
                 $event->sheet->getDelegate()->mergeCells('K2:Q2');
                 $event->sheet->getDelegate()->getStyle('K2:Q2')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+                $event->sheet->getDelegate()->getStyle('K2:Q2')->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+                $event->sheet->getDelegate()->getStyle('K2:Q2')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('00BFFF'); // Color de fondo amarillo
+                $event->sheet->getDelegate()->getStyle('K2:Q2')->getFont()->getColor()->setARGB('000000');
+                $event->sheet->getDelegate()->getStyle('K2:Q2')->getFont()->setSize(18);
 
                 // Combinar celdas para la segunda fila del encabezado PRECIO AL 35%
                 $event->sheet->getDelegate()->mergeCells('R2:T2');
                 $event->sheet->getDelegate()->getStyle('R2:T2')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+                // $event->sheet->getDelegate()->getStyle('R2:T2')->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+                $event->sheet->getDelegate()->getStyle('R2:T2')->getBorders()->getTop()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+                $event->sheet->getDelegate()->getStyle('R2:T2')->getBorders()->getBottom()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+                $event->sheet->getDelegate()->getStyle('R2:T2')->getBorders()->getLeft()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+                $event->sheet->getDelegate()->getStyle('R2:T2')->getBorders()->getRight()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+                $event->sheet->getDelegate()->getStyle('R2:T2')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('00BFFF'); // Color de fondo amarillo
+                $event->sheet->getDelegate()->getStyle('R2:T2')->getFont()->getColor()->setARGB('000000');
+                $event->sheet->getDelegate()->getStyle('R2:T2')->getFont()->setSize(18);
+
+                // Establecer bordes para los datos en la fila 4 (por ejemplo, de A4 a T4)
+                $event->sheet->getDelegate()->getStyle('A3:T3')->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
 
                 // Congelar la primera fila (encabezado principal)
-                $event->sheet->freezePane('A4', 'A4');
+                $event->sheet->freezePane('A4', 'T4');
             },
         ];
     }
-
 
     public function collection()
     {
