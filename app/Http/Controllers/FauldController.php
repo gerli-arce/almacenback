375f59d9973f9bfe9c61eaf13aca6fb24f88f819
gerli-arce <yoninfantearce@gmail.com>
2023-10-26 16:07:51 -0500

    nts

--- app/Http/Controllers/FauldController.php
+++ app/Http/Controllers/FauldController.php
@@ -745,9 +745,8 @@ class FauldController extends Controller
                     ->where('_branch', $branch_->id)
                     ->first();
 
-                $productJpa->status_product = "DISPONIBLE";
+                $productJpa->disponibility = "DISPONIBLE";
                 if ($productJpa->type == "MATERIAL") {
-
                     $productByTechnicalJpa = ProductByTechnical::where('_technical', $saleProductJpa->_technical)
                         ->whereNotNull('status')
                         ->whereNot('type', 'LEND')
@@ -761,6 +760,8 @@ class FauldController extends Controller
                         $stock->mount_new = $stock->mount_new + 1;
                     } else if ($productJpa->product_status == 'SEMINUEVO') {
                         $stock->mount_second = $stock->mount_second + 1;
+                    }else{
+                        $stock->mount_ill_fated = $stock->mount_ill_fated + 1;
                     }
                 }
                 $stock->save();
